6fd167ee86c5cefa480cfccbf9929ef9e92006e8
antoninolatocca <omnial95@gmail.com>
2021-03-02 18:37:20 +0100

    Uso Bootstrap, filtro e ricerca

--- main.js
+++ main.js
@@ -1,66 +1,76 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		pizze: [
+		menu: [
 			{
-				nome: 'Margherita',
-				ingredienti: ['Pomodoro', 'Mozzarella'],
-				descrizione: 'Tipica pizza italiana',
-				immagine: 'img/pizza-margherita.jpg',
-				prezzo: "4.50"
-			}, 
-			{
-				nome: 'Marinara',
-				ingredienti: ['Pomodoro', 'Aglio', 'Acciughe'],
-				descrizione: 'La pizza più semplice',
-				immagine: 'img/pizza-marinara.jpg',
-				prezzo: "4.00"
+				nome: 'Pizze',
+				lista: [
+					{
+						nome: 'Margherita',
+						ingredienti: ['Pomodoro', 'Mozzarella'],
+						descrizione: 'Tipica pizza italiana',
+						immagine: 'img/pizza-margherita.jpg',
+						prezzo: "4.50"
+					}, 
+					{
+						nome: 'Marinara',
+						ingredienti: ['Pomodoro', 'Aglio', 'Acciughe'],
+						descrizione: 'La pizza più semplice',
+						immagine: 'img/pizza-marinara.jpg',
+						prezzo: "4.00"
+					},
+					{
+						nome: 'Tonno e Cipolla',
+						ingredienti: ['Pomodoro', 'Mozzarella', 'Tonno', 'Cipolla'],
+						descrizione: 'La pizza leggera',
+						immagine: 'img/pizza-tonno-e-cipolla.jpg',
+						prezzo: "6.00"
+					}
+				]
 			},
 			{
-				nome: 'Tonno e Cipolla',
-				ingredienti: ['Pomodoro', 'Mozzarella', 'Tonno', 'Cipolla'],
-				descrizione: 'La pizza leggera',
-				immagine: 'img/pizza-tonno-e-cipolla.jpg',
-				prezzo: "6.00"
+				nome: 'Risotti',
+				lista: [
+					{
+						nome: 'Riso cantonese',
+						ingredienti: ['Riso Basmati', 'Piselli', 'Prosciutto cotto', 'Uovo', 'Salsa di soia'],
+						descrizione: 'Riso fritto orientale',
+						immagine: 'img/riso-alla-cantonese.jpg',
+						prezzo: "8.00"
+					},
+					{
+						nome: 'Risotto alla milanese',
+						ingredienti: ['Riso', 'Burro', 'Vino bianco', 'Brodo di carne', 'Cipolla', 'Zafferano', 'Parmiggiano'],
+						descrizione: 'Il risotto allo zafferano',
+						immagine: 'img/risotto-alla-milanese.jpg',
+						prezzo: '8.00'
+					}
+				],
 			}
 		],
-		risotti: [
-			{
-				nome: 'Riso cantonese',
-				ingredienti: ['Riso Basmati', 'Piselli', 'Prosciutto cotto', 'Uovo', 'Salsa di soia'],
-				descrizione: 'Riso fritto orientale',
-				immagine: 'img/riso-alla-cantonese.jpg',
-				prezzo: "8.00"
-			},
-			{
-				nome: 'Risotto alla milanese',
-				ingredienti: ['Riso', 'Burro', 'Vino bianco', 'Brodo di carne', 'Cipolla', 'Zafferano', 'Parmiggiano'],
-				descrizione: 'Il risotto allo zafferano',
-				immagine: 'img/risotto-alla-milanese.jpg',
-				prezzo: '8.00'
-			}
-		],
-		cucina: ['Mozzarella', 'Pomodoro', 'Patatine fritte', 'Rucola', 'Cipolla', 'Acciughe', 'Aglio']
+		cucina: ['Mozzarella', 'Pomodoro', 'Patatine fritte', 'Rucola', 'Cipolla', 'Acciughe', 'Aglio'],
+		filtro: false,
+		search: ""
 	},
     methods: {
-      disponibile: function(piatto, frigo) {
-		  for(var i = 0; i < piatto.length; i++) {
-			  if (!frigo.includes(piatto[i])) {
-				  return false;
-			  }
-		  }
-		  return true;
-      },
-	  esaurisci: function(elemento, cucina) {
-		  for(var i = 0; i < cucina.length; i++){ 
-			  if ( cucina[i] == elemento) { 
-				  cucina.splice(i, 1); 
-			  }
-		  }
-	  },
-	  spesa: function(elemento, cucina) {
-		  cucina.push(elemento.value);
-		  elemento.value = "";
-	  }
+		disponibile: function(piatto, frigo) {
+			for(var i = 0; i < piatto.length; i++) {
+				if (!frigo.includes(piatto[i].toLowerCase())) {
+					return false;
+				}
+			}
+			return true;
+		},
+		esaurisci: function(elemento, cucina) {
+			for(var i = 0; i < cucina.length; i++){ 
+				if ( cucina[i] == elemento) { 
+					cucina.splice(i, 1); 
+				}
+			}
+		},
+		spesa: function(elemento, cucina) {
+			cucina.push(elemento.value);
+			elemento.value = "";
+		}
     }
 });
